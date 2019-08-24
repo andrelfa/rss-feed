70c109c4ef40f8f2bf9e153a82f802a7e66387e7
André Luiz Ferreira Antunes <andrelfantunes@gmail.com>
2019-08-24 15:33:35 -0300

    Ajustes filtros

--- src/App.js
+++ src/App.js
@@ -5,38 +5,70 @@ import RssItemCard from './components/RssItemCard';
 import { sortByDateWithMoment } from './utils/utils';
 
 const App = () => {
-
+  // const [bbcFeed, setBBCFeed] = useState([]);
+  // const [kotakuFeed, setKotakuFeed] = useState([]);
   const [feed, setFeed] = useState([]);
 
   useEffect(() => {
-    Promise.all([Service.getBBCFeed(), Service.getIGNFeed()])
-    .then((res) => {
-      if (!feed.length) {
-        console.log('res', res[1]);
-        const [bbcFeed, IGNFeed] = res;
-        setFeed(sortByDateWithMoment([...bbcFeed, ...IGNFeed], 'publishDate'));
+      const fetchData = async () => {
+        // await Promise.all([Service.getBBCFeed(), Service.getKotakuFeed(), Service.getB9Feed()])
+        Service.getBBCFeed().then(res => setFeed((old) => sortByDateWithMoment([...old,...res], 'publishDate')));
+        Service.getKotakuFeed().then(res => setFeed((old) => sortByDateWithMoment([...old, ...res], 'publishDate')));
+        Service.getB9Feed().then(res => setFeed((old) => sortByDateWithMoment([...old, ...res], 'publishDate')));
       }
-    }) 
-  }, [feed]);
+
+      fetchData();
+  }, []);
 
 
 
   const feedItems = () => {
     return feed.map((item) => {
-      return (<RssItemCard 
+      return item.active ? (<RssItemCard 
         key={item.link} 
         title={item.title} 
         description={item.description} 
         link={item.link}
         publishDate={item.publishDate}
-      />)
+      />) : null
     })
+  }
+
+  const handleChangeFilter = (name) => {
+    // const hasItemsWithName = !!feed.filter(item => item.feedName === name).length;
+    // const newFeed = [...feed].filter(item => hasItemsWithName ? item.feedName !== name : item.feedName === name);
+    // setFeed([...newFeed]);
+    // console.log('new feed', newFeed)
+
+    const newFeed = [...feed].map((item) => {
+      if (item.feedName === name) item.active = !item.active;
+      return item;
+    })
+    setFeed([...newFeed]);
   }
 
   return (
     <div className="App">
-      <div className="feed-container">
-        {feed.length && feedItems()}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 div col-sm-12">
+            <div className="filter-container">
+              <p className="filter-title">
+                Filtros:
+              </p>
+              <div className="filter-buttons">
+                <button className={`filter-btn ${feed.filter(item => item.feedName === 'Kotaku' && item.active === true).length > 0 ? 'checked' : ''}`} onClick={() => handleChangeFilter('Kotaku')}>
+                  Kotaku
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12 col-md-12">
+            {feed.length && feedItems()}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/services/rss-feeds.js
+++ src/services/rss-feeds.js
@@ -11,16 +11,18 @@ class RSSFeedService {
         .then(response => convert.xml2js(response, { compact: true, spaces: 4 }).rss.channel.item)
         .then(response => response.map((item) => {
             return {
+                feedName: 'BBCUK',
                 description: item.description._cdata,
                 title: `BBC UK - ${item.title._cdata}`,
                 link: item.link._text,
-                publishDate: moment(item.pubDate._text).format('YYYY-MM-DD HH:mm')
+                publishDate: moment(item.pubDate._text).format('YYYY-MM-DD HH:mm'),
+                active: true
             }
         }))
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
     }
 
-    getIGNFeed = () => {
+    getKotakuFeed = () => {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
         const url = "http://kotaku.com/vip.xml"; // site that doesn’t send Access-Control-*
         return fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
@@ -28,13 +30,34 @@ class RSSFeedService {
         .then(response => convert.xml2js(response, { compact: true, spaces: 4 }).rss.channel.item)
         .then(response => response.map((item) => {
             return {
+                feedName: 'Kotaku',
                 description: item.description._cdata,
                 title: `Kotaku - ${item.title._text}`,
                 link: item.link._text,
-                publishDate: moment(item.pubDate._text).format('YYYY-MM-DD HH:mm')
+                publishDate: moment(item.pubDate._text).format('YYYY-MM-DD HH:mm'),
+                active: true
             }
         }))
         .then(response => response.filter(item => item.link.indexOf('kotaku') > -1))
+        .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    }
+
+    getB9Feed = () => {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "http://www.b9.com.br/feed/"; // site that doesn’t send Access-Control-*
+        return fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+        .then(response => response.text())
+        .then(response => convert.xml2js(response, { compact: true, spaces: 4 }).rss.channel.item)
+        .then(response => response.map((item) => {
+            return {
+                feedName: 'B9',
+                description: item.description._cdata,
+                title: `B9 - ${item.title._text}`,
+                link: item.link._text,
+                publishDate: moment(item.pubDate._text).format('YYYY-MM-DD HH:mm'),
+                active: true
+            }
+        }))
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
     }
 }
